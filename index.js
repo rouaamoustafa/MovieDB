36d2d08d9058d66097e623b34fe808c78b744c4c
RouaaM <moustafarouaa@gmail.com>
2024-12-23 00:37:05 +0200

    step 5 -adding Create ,Read,Delete,Update to Movies route

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express'); // Import Express
 const app = express(); // Create an Express app
 const port = 3000; // Define the port your server will listen on
 
+
+//Step 3
 // Route: /test
 app.get('/test', (req, res) => {
   res.json({
@@ -19,7 +21,8 @@ app.get('/time', (req, res) => {
     message: time
   });
 });
-
+//Step 4
+//1
 // Route: /hello/:id
 app.get('/hello/:id?', (req, res) => {
   const id = req.params.id || "Guest"; // Get 'id' from the URL or default to 'Guest'
@@ -29,7 +32,7 @@ app.get('/hello/:id?', (req, res) => {
   });
 });
 
-
+//2
 // Route: /search
 app.get('/search', (req, res) => {
   const search = req.query.s; // Extract 's' query parameter from the URL
@@ -50,6 +53,44 @@ app.get('/search', (req, res) => {
     });
   }
 });
+
+//Step 5
+//1
+const movies = [
+  { title: 'Jaws', year: 1975, rating: 8 },
+  { title: 'Avatar', year: 2009, rating: 7.8 },
+  { title: 'Brazil', year: 1985, rating: 8 },
+  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+// Route: /movies/create
+app.get('/movies/create', (req, res) => {
+  res.send('Movies: Create route');
+});
+
+// Route: /movies/read
+app.get('/movies/read', (req, res) => {
+  res.json({
+    status: 200,
+    data:movies,
+  });
+   
+});
+
+// Route: /movies/update
+app.get('/movies/update', (req, res) => {
+  res.send('Movies: Update route');
+});
+
+// Route: /movies/delete
+app.get('/movies/delete', (req, res) => {
+  res.send('Movies: Delete route');
+});
+
+
+
+
+
+
 
 // Define a fallback route for unmatched URLs
 app.use((req, res) => {
